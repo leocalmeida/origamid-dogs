4983a09d3a3ef4edd5c7483b15f22a041cc0b20a
FIRST_NAME LAST_NAME <leonardocaetanoalmeida@gmail.com>
2022-06-15 11:48:33 -0300

    iniciado o uso do contexto na aplicacao

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login/*' element={<Login />} />
-      </Routes>
-      <Footer />
+      <UserStorage>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login/*' element={<Login />} />
+        </Routes>
+        <Footer />
+      </UserStorage>
     </BrowserRouter>
   );
 }
