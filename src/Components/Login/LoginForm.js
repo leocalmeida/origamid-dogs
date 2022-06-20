7b3873c375114e98c966e2ee4a14874c42a64f01
FIRST_NAME LAST_NAME <leonardocaetanoalmeida@gmail.com>
2022-06-20 12:17:45 -0300

    adicionado os estilos a pagina de login, incluindo a foto do caozinho.

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../../Components/Forms/Button.module.css';
 
 const LoginForm = () => {
   const username = useForm();
@@ -19,9 +22,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <section className='animeLeft'>
+      <h1 className='title'>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label='Usuário' type='text' name='username' {...username} />
         <Input label='Senha' type='password' name='password' {...password} />
         {loading ? (
@@ -29,10 +32,20 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
+        <Error error={error} />
 
-        {error && <p>{error}</p>}
         <Link to='/login/criar'>Cadastro</Link>
       </form>
+      <Link to='/login/perdeu' className={styles.perdeu}>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui contar? Cadastre-se no site</p>
+        <Link className={stylesBtn.button} to='/login/criar'>
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
